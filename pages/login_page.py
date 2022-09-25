f6cd9e0d2f0c7763011d643cc02750d708ad2a04
peterhan <hpete3454@gmail.com>
2022-09-26 03:20:47 +0900

    All feature done

--- pages/login_page.py
+++ pages/login_page.py
@@ -9,9 +9,6 @@ class LoginPage(BasePage):
     _submit_button = {"by": By.CSS_SELECTOR, "value": "button"}
     _success_message = {"value": "Dashboard"}
     _login_page_title = {"value": "Login"}
-    _accounts_button = {"by": By.XPATH, "value": '//*[@id="drawerAccordion"]/div/div/a[6]'}
-    _customers_button = {"by": By.LINK_TEXT, "value": "Customers"}
-    _customers_page_title = {"value": "Customers"}
 
     def __init__(self, driver):
         self.driver = driver
@@ -23,19 +20,5 @@ class LoginPage(BasePage):
         self._type(self._password_input, password)
         self._click(self._submit_button)
 
-    def login_success_check(self, firstname, lastname, email, password, mobile, country, address1, address2):
+    def login_success_check(self, timeout=0):
         return self._get_title(self._success_message, config.login_success_timeout)
-
-    # def goto_customers_page(self):
-    #     self._click(self._accounts_button)
-    #     self._click_inarow(self._customers_button, config.login_success_timeout)
-    #     return self._get_title(self._customers_page_title, config.login_success_timeout)
-
-
-    #
-    # def failure_message_present(self):
-    #     return self._is_displayed(self._failure_message, 1)
-
-    # def test_invalid_credentials(self, login):
-    #     login.with_("tomsmith", "bad password")
-    #     assert login.failure_message_present()
